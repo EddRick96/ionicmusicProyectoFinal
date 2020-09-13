7f8af49a2a5775bffc1b6914b438b7b431522109
EddRick96 <erick.bolanos@epn.edu.ec>
2020-09-13 05:34:40 -0500

    Inicializar proyecto final

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,16 +8,16 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        Hola, Erick!
       </IonContent>
     </IonPage>
   );
